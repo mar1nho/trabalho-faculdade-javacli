bdad1b14e82c0c0d58b24fb685515323640c4bfd
mar1nho <gustavomarinhosom@gmail.com>
2025-05-02 16:11:04 -0300

    Tela de login e registro criados, preciso atualizar o UserModel para portar um tipo de endereço para ele puxar no aplicativo os eventos próximos, endereço tipo campus ou algo assim.

--- src/main/java/cli/LoginCLI.java
+++ src/main/java/cli/LoginCLI.java
@@ -1,22 +1,93 @@
 package cli;
 
+import models.UserModel;
 import services.UserService;
 import utils.Log;
 
+import java.util.HashMap;
+import java.util.Optional;
+
 public class LoginCLI {
+
+    private UserService userService;
+
+    public LoginCLI() {
+        userService = new UserService();
+    }
 
     public void mainScreen(){
         Log.ascii();
-        Log.info("Olá, entre abaixo:");
-        Log.input("Usuário: ");
-        String username = Log.getUserInput();
-        Log.input("Senha: ");
-        String password = Log.getUserInput();
-        UserService userService = new UserService();
-        boolean result = userService.loginAuthentication(username, password);
-        System.out.println(result);
+        Log.info("Olá, seja bem-vindo ao 'TáRolando?'");
+        Log.info("Escolha entre as opções abaixo:");
+        Log.info("1. Entrar");
+        Log.info("2. Registrar");
+        Log.input(" ");
+        String response = Log.getUserInput();
+        switch(response){
+            case "1"->{
+                loginScreen();
+            }
+            case "2"->{
+                registerScreen();
+            }
+            default -> {
+                Log.info("Nenhuma opção reconhecida na sua resposta...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                mainScreen();
+            }
+        }
     }
 
+    private void registerScreen(){
+        Log.ascii();
+        Log.info("Iremos agora criar uma conta com apenas 2 informações!");
+        Log.input("Username: ");
+        String usr = Log.getUserInput();
+        Log.input("Password: ");
+        String pwd = Log.getUserInput();
+        if(userService.saveUser(new UserModel(usr,pwd, new HashMap<>()))){
+            Log.info("Parabéns, conta criada com sucesso!");
+            Log.info("Retornando ao menu.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            mainScreen();
+        } else {
+            Log.info("Usuário inválido, tente novamente!");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            registerScreen();
+        }
+    }
 
+    public void loginScreen(){
+        Log.ascii();
+        Log.info("Por favor, entre com suas credenciais abaixo.");
+        Log.input("Username: ");
+        String usr = Log.getUserInput();
+        Log.input("Password: ");
+        String pwd = Log.getUserInput();
+
+        boolean _auth_ = userService.loginAuthentication(usr,pwd);
+        if(_auth_){
+            Optional<UserModel> user = userService.getAllUsers()
+                    .stream()
+                    .filter(u -> usr.equals(u.getUsername()))
+                    .findFirst();
+            if(user.isPresent()){
+                UserScreenCLI userScreen = new UserScreenCLI(user.get());
+                userScreen._initialize();
+            }
+        }
+    }
 
 }
--- src/main/java/cli/UserScreenCLI.java
+++ src/main/java/cli/UserScreenCLI.java
@@ -1,4 +1,54 @@
 package cli;
 
+import models.EventModel;
+import models.UserModel;
+import services.EventService;
+import utils.Log;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class UserScreenCLI {
+
+    private EventService eventService;
+
+    private UserModel mainUser;
+
+    public UserScreenCLI(UserModel user) {
+        this.mainUser = user;
+        this.eventService = new EventService();
+    }
+
+    public void _initialize(){
+        Runnable runnable = () -> {
+            Log.ascii();
+            Log.info("Welcome to the user screen");
+            testCalculate();
+        };
+        runnable.run();
+    }
+
+
+    public void testCalculate() {
+        List<EventModel> eventModelList = eventService.loadEventListAsModel();
+
+        LocalDateTime now = LocalDateTime.now();
+        List<LocalDateTime> eventsTime = new ArrayList<>();
+        eventModelList.forEach(eventModel -> {
+            eventsTime.add(eventModel.getDate());
+        });
+
+        long pastEvents = eventsTime.stream()
+                .filter(dataEvento -> dataEvento.isBefore(now))
+                .count();
+
+        long futureEvents = eventsTime.stream()
+                .filter(dataEvento -> dataEvento.isAfter(now))
+                .count();
+
+        Log.info("Total of ended events since launch: " + pastEvents);
+        Log.info("Future events coming: " + futureEvents);
+    }
 }
